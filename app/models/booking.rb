fbf0316aed293f128015bc574418f7d8304d1161
Abdul Rafay <abdulrafayn001@gmail.com>
2024-03-06 12:26:01 -0800

    Add validation to the booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,6 @@
 class Booking < ApplicationRecord
   enum animal_type: { dog: 0, cat: 1 }
+
+  validates :first_name, :last_name, :animal_name, :animal_type, :hours_requested, :date_of_service, presence: true
+  validates :hours_requested, numericality: { greater_than_or_equal_to: 2, less_than_or_equal_to: 8 }
 end
